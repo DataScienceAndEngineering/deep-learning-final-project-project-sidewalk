46f77446d2bb1d7428665db2dfae1308bbf43faa
NickL99 <32443489+NicholasLeotta99@users.noreply.github.com>
2023-05-13 14:37:53 -0400

    Cleaned up code, expanded function
    Randomly augments training data.  Can set label and thresh parameter to adjust object utilized as obstruction

--- notebooks/augment_obstructions.py
+++ notebooks/augment_obstructions.py
@@ -9,78 +9,103 @@ import json
 from PIL import Image, ImageDraw
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+import random
 
-json_path = '/home/nicholas/Documents/Project_Sidewalk/Project_Sidewalk/gtFine/train/aachen/aachen_000170_000019_gtFine_polygons.json'
-img_path = '/home/nicholas/Documents/Project_Sidewalk/Project_Sidewalk/leftImg8bit/train/aachen/aachen_000170_000019_leftImg8bit.png'
-image = Image.open(img_path)
-plt.imshow(image)
-instance_data = json.load(open(json_path))
+def build_masks(instance_data, label_keep):
+    #converts json data into masks for all labels requested
+    img = Image.new('L', (instance_data['imgWidth'], instance_data['imgHeight']), 0)
+    img_map = {}
+    j=1
+    for i in instance_data['objects']:
+        if i['label'] in label_keep.keys():
+            polygon = i['polygon']
+            xy = [(p[0], p[1]) for p in polygon]
+            ImageDraw.Draw(img).polygon(xy,fill=j)
+            img_map[j] = i['label']
+            j+=1
+    return img, img_map
+
+def isolate_mask(img, img_map, label='pole', thresh=2000):
+    #Isolates a mask of label object over a provided threshold size
+    k = 0
+    extracted_car = np.empty((np.array(img).shape[0], np.array(img).shape[1], 3))
+    for i in img_map.values():
+        if i == label:
+            idx = (np.array(img)==list(img_map.keys())[k])
+            if idx.sum() > thresh:
+                extracted_car[idx] = np.array(image)[idx]
+                return extracted_car
+        k+=1
+    raise Exception(f"No mask for {label} found over threshold of {thresh}")
+
+def crop(image):
+    #Removes all padding zeros, cropping to smallest size
+    nonzero_map = np.nonzero(image)
+    min_indices = np.min(nonzero_map, axis=1)
+    max_indices = np.max(nonzero_map, axis=1)
+    cropped = image[min_indices[0]:max_indices[0]+1, min_indices[1]:max_indices[1]+1, :]
+    return np.clip(cropped, 0, 255).astype(np.uint8)
+
+def locate_dest(img, img_map):
+    sidewalks = list()
+    idx_sums = list()
+    k=0
+    for i in img_map.values():
+        if i == 'sidewalk':
+            sidewalks.append(np.array(img) == list(img_map.keys())[k])
+            idx_sums.append(np.sum(sidewalks[-1])) 
+        k+=1
+    largest = sidewalks[np.argwhere(idx_sums == np.max(idx_sums))[0][0]]
+    x, y = np.where(largest)
+    median_location = np.median(np.column_stack((y,x)),axis=0)
+    return (median_location[1],median_location[0])
+
+def paste(base, img, destination):
+    # Crop the object array to remove padding zeros
+    [size_0, size_1] = img.shape[:-1]
+    median_0 = int(destination[0])
+    median_1 = int(destination[1])
+    while size_1+median_1 > 2048:
+        size_1 -= 1
+    while median_0-size_0 < 0:
+        size_0 -= 1
+    base = np.array(base)
+    orig = np.array(base)
+    cropped = img[0:size_0, 0:size_1]
+    base[median_0-size_0:median_0, median_1:median_1+size_1,:] = cropped
+    for i in range(median_0-size_0,median_0):
+        for j in range(median_1,median_1+size_1):
+            if (np.sum(base[i,j,:]) == 0):
+                base[i,j,:] = orig[i,j,:]
+    return base
+
+def json2img(file):
+    return file.replace('_gtFine_polygons.json', '_leftImg8bit.png')
+
+json_path = '/home/nicholas/Documents/Project_Sidewalk/Project_Sidewalk/json/train/'
+img_path = '/home/nicholas/Documents/Project_Sidewalk/Project_Sidewalk/images_RAW/train/'
+jsons = os.listdir(json_path)
+imgs = os.listdir(img_path)
+
+json_file = random.choice(jsons)
+img_file = json2img(json_file)
+json_fpath = json_path+json_file
+img_fpath = img_path+img_file
+image = Image.open(img_fpath)
+instance_data = json.load(open(json_fpath))
 label_keep = {
     'sidewalk': 8,
     'pole': 17,
     'car': 26,
     'bicycle': 33
     }
-img = Image.new('L', (2048, 1024), 0)
-j=1
-img_map = {}
-for i in instance_data['objects']:
-    if i['label'] in label_keep.keys():
-        polygon = i['polygon']
-        xy = [(p[0], p[1]) for p in polygon]
-        ImageDraw.Draw(img).polygon(xy,fill=j)
-        img_map[j] = i['label']
-        j+=1
-k =0
-extracted_car = np.empty((1024, 2048, 3))
-for i in img_map.values():
-    if i == 'car':
-        idx = (np.array(img)==list(img_map.keys())[k])
-        if idx.sum() > 12000:
-            extracted_car[idx] = np.array(image)[idx]
-            break
-    k+=1
+       
+img, img_map = build_masks(instance_data, label_keep)
+extracted_car = isolate_mask(img, img_map, label='car', thresh=12000)
+car = crop(extracted_car)
+destination = locate_dest(img, img_map)     
+output = paste(image, car, destination)
 
-        
-nonzero_indices = np.nonzero(extracted_car)
-min_indices = np.min(nonzero_indices, axis=1)
-max_indices = np.max(nonzero_indices, axis=1)
-# Crop the object array to remove padding zeros
-cropped = extracted_car[min_indices[0]:max_indices[0]+1, min_indices[1]:max_indices[1]+1, ...]
-
-cropped = np.clip(cropped, 0, 255).astype(np.uint8)
-plt.imshow(cropped)
-
-car_size = cropped.shape[:-1]
-k=0
-sidewalks=list()
-idx_sums = list()
-idx = list()
-for i in img_map.values():
-    if i == 'sidewalk':
-        mask = np.array(img) == list(img_map.keys())[k]
-        sidewalks.append(mask)
-        idx_sums.append(np.sum(mask))
-    k+=1
-largest_walk = sidewalks[np.argwhere(idx_sums == np.max(idx_sums))[0][0]]
-
-y, x = np.where(largest_walk)
-median_location = np.median(np.column_stack((y, x)), axis=0)
-median_0 = int(median_location[0])
-median_1 = int(median_location[1])
-print(car_size)
-car_1 = car_size[1]
-car_0 = car_size[0]
-while car_1+median_1 > 2048:
-    car_1 -= 1
-while median_0-car_0 < 0:
-    car_0 -= 1
-np_image = np.array(image)
-cropped = cropped[0:car_0, 0:car_1]
-np_image[median_0-car_0:median_0, median_1:median_1+car_1] = cropped
-for i in range(median_0-car_0,median_0):
-    for j in range(median_1,median_1+car_1):
-        if (np_image[i,j] == 0).all():
-            np_image[i,j] = np.array(image)[i,j]
-plt.imshow(cropped)
-plt.imshow(np_image)
+plt.imshow(car)
+plt.imshow(output)
